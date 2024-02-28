2052c53546aa2a752be80a919198ee184073bd6a
Dominik Köppl <git@dkppl.de>
2024-02-28 09:36:27 +0000

    de-duplicate galois factorization

--- galois_factorization.cpp
+++ galois_factorization.cpp
@@ -1,84 +1,62 @@
 #include "common.hpp"
 #include "dcheck.hpp"
 
-std::vector<std::string> linearGaloisFactorization(const std::string& s) {
-  std::string t = s;
-  t += '$';
+constexpr bool odd_less(const char& a, const char& b) {
+  return a < b;
+}
+
+constexpr bool even_less(const char& a, const char& b) {
+  return a > b;
+}
+
+std::vector<std::string> galois_factorization(const std::string& s) {
+  if(s.find('\0') != std::string::npos) {
+    throw std::runtime_error(std::string("input string must not contain NULL byte"));
+  }
+  std::string text = s;
+  text += '\0';
   std::vector<std::string> ans;
   size_t i = 0;
-  while (i < t.size() - 1) {
+  while (i < text.size() - 1) {
     size_t po = 1;
     size_t pe = 2;
-    for (size_t j = 1; j < t.size() - i; j++) {
-      if (j % 2 == 0) {  // odd position
-        size_t p = t.size() + 1;
-        size_t pe2 = pe;
-        size_t po2 = po;
-        if (pe <= j) {
-          if (t[i + j] < t[i + j - pe]) {
-            p = std::min(p, pe);
-          } else if (t[i + j] > t[i + j - pe]) {
-            pe2 = j + 2;
-          }
+    for (size_t j = 1; j < text.size() - i; j++) {
+      const bool is_odd = (j % 2 == 0);
+      auto smaller_than = is_odd ? odd_less : even_less;
+
+      size_t p = text.size() + 1;
+      size_t pe2 = pe;
+      size_t po2 = po;
+      if (pe <= j) {
+        if (smaller_than(text[i + j], text[i + j - pe])) {
+          p = std::min(p, pe);
+        } else if (smaller_than(text[i + j - pe], text[i + j])) {
+          pe2 = j + 1 + (is_odd ? 1 : 0);
         }
-        if (po <= j) {
-          if (t[i + j] < t[i + j - po]) {
-            po2 = j + 1;
-          } else if (t[i + j] > t[i + j - po]) {
-            p = std::min(p, po);
-          }
-        }
-        if (p != t.size() + 1) {
-          while (j >= p) {
-            if (p == pe && pe == 2 * po) {
-              ans.push_back(t.substr(i, po));
-              ans.push_back(t.substr(i + po, po));
-            } else {
-              ans.push_back(t.substr(i, p));
-            }
-            i += p;
-            j -= p;
-            p = pe;
-          }
-          j = t.size();
-        }
-        pe = pe2;
-        po = po2;
-      } else {  // even position
-        size_t p = t.size() + 1;
-        size_t pe2 = pe;
-        size_t po2 = po;
-        if (pe <= j) {
-          if (t[i + j] > t[i + j - pe]) {
-            p = std::min(p, pe);
-          } else if (t[i + j] < t[i + j - pe]) {
-            pe2 = j + 1;
-          }
-        }
-        if (po <= j) {
-          if (t[i + j] > t[i + j - po]) {
-            po2 = j + 2;
-          } else if (t[i + j] < t[i + j - po]) {
-            p = std::min(p, po);
-          }
-        }
-        if (p != t.size() + 1) {
-          while (j >= p) {
-            if (p == pe && pe == 2 * po) {
-              ans.push_back(t.substr(i, po));
-              ans.push_back(t.substr(i + po, po));
-            } else {
-              ans.push_back(t.substr(i, p));
-            }
-            i += p;
-            j -= p;
-            p = pe;
-          }
-          j = t.size();
-        }
-        pe = pe2;
-        po = po2;
       }
+      if (po <= j) {
+        if (smaller_than(text[i + j], text[i + j - po])) {
+          po2 = j + 1 + (is_odd ? 0 : 1);
+        } else if (smaller_than(text[i + j - po], text[i + j])) {
+          p = std::min(p, po);
+        }
+      }
+      if (p != text.size() + 1) {
+        while (j >= p) {
+          if (p == pe && pe == 2 * po) {
+            ans.push_back(text.substr(i, po));
+            ans.push_back(text.substr(i + po, po));
+          } else {
+            ans.push_back(text.substr(i, p));
+          }
+          i += p;
+          j -= p;
+          p = pe;
+        }
+        j = text.size();
+      }
+      pe = pe2;
+      po = po2;
     }
   }
   return ans;
@@ -110,9 +88,13 @@ bool check_factorization(const std::vector<std::string> factor) {
   return true;
 }
 
-int main() {
-  const auto factors = linearGaloisFactorization(read_input());
-  showFactorization(factors);
+int main(int argc, char *argv[]) {
+  const auto factors = galois_factorization(read_input());
   DCHECK(check_factorization(factors));
+  if(argc > 1 && std::string(argv[1]) == "count") {
+    std::cout << factors.size();
+  } else {
+  showFactorization(factors);
+  }
   return 0;
 }
